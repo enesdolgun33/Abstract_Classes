7bafe28d2337028615367b001101d8fcef148e0a
enesdolgun33 <enesdolgun33@gmail.com>
2025-01-31 08:00:08 +0300

    commit

---
+++ src/OlderGameCal.java
@@ -0,0 +1,7 @@
+public class OlderGameCal extends GameCalculator{
+
+    @Override
+    public void hesapla() {
+        System.out.println("puanınız 31");
+    }
+}
---
+++ src/WomanGameCal.java
@@ -0,0 +1,7 @@
+public class WomanGameCal extends GameCalculator{
+
+    @Override
+    public void hesapla(){
+        System.out.println("Puanınız : 55");
+    }
+}
